0a5f231ef12a4d677759b1f49c8cc1ce5d4a55e7
ewerton <ewerton.lima@sptech.school>
2022-12-21 16:27:09 -0300

    arquivos html css & javascript

---
+++ Curso_em_Video/4-Estudos Durante o Curso JavaScript/codicoes.js
@@ -0,0 +1,9 @@
+var velocidade = 60.5
+
+console.log(`A Velocidade do seu carro é ${velocidade} KM`)
+
+
+if(velocidade >60){ // condição simples
+    console.log("Você ultrapassou a velocidade e foi multado")
+}
+console.log(`Dirija com cuidado`)
---
+++ Curso_em_Video/4-Estudos Durante o Curso JavaScript/condicoes02.js
@@ -0,0 +1,10 @@
+// condições composta
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log(" Você éBrasileiro")
+}
+else{
+    console.log("Você é Estrangeiro")
+}
